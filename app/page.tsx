bd0894f3d0de8d4fd802307cf00215467fc50511
krapotur <akemran@yandex.ru>
2024-04-20 18:54:46 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -20,30 +20,12 @@ export default function Home() {
       <HeroSection />
       <Content />
       <Rating rating={rating} isEditable setRating={setRating} />
-      <Paragraph size="l">Это новый элемент</Paragraph>
-      <Heading tag="h1">Это новый элемент</Heading>
-      <Heading tag="h2">Это новый элемент</Heading>
-      <Heading tag="h3">Это новый элемент</Heading>
       <Button
         appearance="primary"
         arrow="down"
         className="flex gap-2 items-center flex-row p-5"
       >
         Кнопка 1
-      </Button>
-
-      <Button
-        appearance="primary"
-        arrow="right"
-        className="flex gap-2 items-center flex-row p-5"
-      >
-        Кнопка 2
-      </Button>
-      <Button
-        appearance="primary"
-        className="flex gap-2 items-center flex-row p-5"
-      >
-        Кнопка 3
       </Button>
     </main>
   );
