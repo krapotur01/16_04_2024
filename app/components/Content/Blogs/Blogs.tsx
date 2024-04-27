08b2c5c2d91909a7c9c2052d271ede60a4204546
krapotur <akemran@yandex.ru>
2024-04-27 19:27:33 +0300

    fix names components Blogs and Testimonials

--- app/components/Content/Blogs/Blogs.tsx
+++ app/components/Content/Blogs/Blogs.tsx
@@ -4,9 +4,9 @@ import { Cards } from './Cards/Cards';
 
 export const Blogs = () => {
   return (
-    <div className='flex flex-col mt-28 items-center w-full'>
-      <Heading tag="h2">Our Latest Articles</Heading>
-	  <Cards />
+    <div className="flex flex-col mt-28 items-center w-full">
+      <Heading tag="h2">OUR LATEST ARTICLES</Heading>
+      <Cards />
     </div>
   );
 }
